3e0b0f8ec4148b2e159467cd778cb589e84aaf00
amolina20 <dbgthd413@gmail.com>
2025-09-06 01:39:21 -0400

    Delete src/main/java/com/bne/postulaciones_service/shared/exceptions/ConflictException.java

--- src/main/java/com/bne/postulaciones_service/shared/exceptions/ConflictException.java
+++
@@ -1,7 +0,0 @@
-package com.bne.postulaciones_service.shared.exceptions;
-
-public class ConflictException extends RuntimeException {
-    public ConflictException(String message) {
-        super(message);
-    }
-}
