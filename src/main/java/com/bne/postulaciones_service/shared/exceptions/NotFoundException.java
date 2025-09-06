61c0767b461832662f5b213caa624fc6f153a613
amolina20 <dbgthd413@gmail.com>
2025-09-06 01:39:38 -0400

    rama actualizada con main

--- src/main/java/com/bne/postulaciones_service/shared/exceptions/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.bne.postulaciones_service.shared.exceptions;
-
-public class NotFoundException extends RuntimeException {
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
