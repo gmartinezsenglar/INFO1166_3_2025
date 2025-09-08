09c38cdf2a81d662037bd634cc5dd676c73d2512
Gustavo Martinez S <gmartinez2021@alu.uct.cl>
2025-09-08 18:35:15 -0300

    entities y VOs

---
+++ src/main/java/com/bne/postulaciones_service/domain/model/Ubicacion.java
@@ -0,0 +1,21 @@
+package com.bne.postulaciones_service.domain.model;
+
+import jakarta.persistence.Embeddable;
+import lombok.*;
+
+@Embeddable
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Ubicacion {
+
+    private String region;
+    private String ciudad;
+
+    public boolean esValida() {
+        return region != null && !region.isBlank() &&
+                ciudad != null && !ciudad.isBlank();
+    }
+}
