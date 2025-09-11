c04df6fc8c93300ffcbda7bcd25995507acf7dc8
amolina20 <dbgthd413@gmail.com>
2025-09-11 19:44:29 -0300

    Pequeño detalle en PostulacionRequestDto

--- src/main/java/com/bne/postulaciones_service/application/dto/PostulacionRequestDto.java
+++ src/main/java/com/bne/postulaciones_service/application/dto/PostulacionRequestDto.java
@@ -3,6 +3,7 @@ package com.bne.postulaciones_service.application.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import com.bne.postulaciones_service.domain.model.vo.OfertaId;
 
 @Data
 @NoArgsConstructor
@@ -10,4 +11,5 @@ import lombok.NoArgsConstructor;
 public class PostulacionRequestDto {
     private Long usuarioId;
     private Long empresaId;
+    private OfertaId ofertaId;
 }
